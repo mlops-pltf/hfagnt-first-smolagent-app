8f78b0cfc6627623a8e2d4c57bba37ba1bba24e3
Saumalya Sarkar <saumalya75@gmail.com>
2025-04-29 00:39:09 +0530

    :bug: MLAIHO-48:Model provider value is ipdated in inference part

--- app.py
+++ app.py
@@ -40,10 +40,11 @@ final_answer = FinalAnswerTool()
 # model_id='https://pflgm2locj2t89co.us-east-1.aws.endpoints.huggingface.cloud' 
 
 model = HfApiModel(
-max_tokens=2096,
-temperature=0.5,
-model_id='Qwen/Qwen2.5-Coder-32B-Instruct',# it is possible that this model may be overloaded
-custom_role_conversions=None,
+    max_tokens=2096,
+    temperature=0.5,
+    model_id='Qwen/Qwen2.5-Coder-32B-Instruct',# it is possible that this model may be overloaded
+    custom_role_conversions=None,
+    provider="together"
 )
 
 
